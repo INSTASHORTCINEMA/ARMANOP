49fbb618498b59ddc1c108b7bfa6859a7c8f1b5f
INSTASHORTCINEMA <155711729+INSTASHORTCINEMA@users.noreply.github.com>
2024-02-08 17:17:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,6 +96,7 @@ SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "seas
 
 
 # Online Stream and Download
+PORT = int(environ.get('PORT', 8080))
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
@@ -105,8 +106,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://armanop-97e83e5cdc49.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://armanop-97e83e5cdc49.herokuapp.com/".format(FQDN, PORT)
+URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "http://{}:{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -121,10 +122,11 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://armanop-97e83e5cdc49.herokuapp.com/".format(FQDN)
+    URL = "https://{}/".format(FQDN)
 else:
-    URL = "https://armanop-97e83e5cdc49.herokuapp.com/".format(FQDN)
-
+    URL = "http://{}/".format(FQDN)
+BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001987654567")).split())) 
+OWNER_USERNAME = "ARMAN_PRO_BOY"
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
